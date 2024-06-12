1ccb504889e44cce6641a743ccc4a1f2fc741cc0
Musaddek Ali <mussali.one@gmail.com>
2024-06-12 16:13:58 +0000

    Install dj-database and psycopg2. Create postgresql and link to project.

--- film_club/settings.py
+++ film_club/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c+80ntl1$o)czyz_)i@w=39s+c&n1d&z1nh622epp4_d70l^k1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-stringermus-djangofilmc-q5xor8d107w.ws-eu114.gitpod.io',
-    '.herokuapp.com'
+    '8000-stringermus-djangofilmc-q5xor8d107w.ws-eu114.gitpod.io', '.herokuapp.com'
 ]
 
 
@@ -77,11 +80,15 @@ WSGI_APPLICATION = 'film_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
 
