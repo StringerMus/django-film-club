43e6694047c63d4664bfca87393b6c98ef259ac2
Musaddek Ali <mussali.one@gmail.com>
2024-06-13 16:01:36 +0000

    Create comment model in 'film'.'

--- film/models.py
+++ film/models.py
@@ -15,3 +15,11 @@ class Review(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
