4adb07eb721183268a9b04c783307a2618bc1d08
Musaddek Ali <mussali.one@gmail.com>
2024-06-15 10:12:00 +0000

    admin.py -  Edit FilmAdmin class filters and fields , create ReviewAdmin and CommenAdmin classes.

--- film/admin.py
+++ film/admin.py
@@ -6,14 +6,32 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Movie)
 class FilmAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'year', 'genre')
+    list_display = ('title', 'slug', 'year', 'genre', 'director')
     search_fields = ['title', 'genre']
-    list_filter = ()
+    list_filter = ('added_on', 'genre',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('synopsis',)
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'film', 'author', 'status', 'created_on', 'updated_on')
+    search_fields = ['title', 'film', 'author']
+    list_filter = ('film', 'created_on', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('review', 'author', 'created_on')
+    search_fields = ['review', 'author']
+    list_filter = ('review', 'created_on',)
+    prepopulated_fields = {}
+    summernote_fields = ('body',)
+
+
 # Register your models here.
-admin.site.register(Review)
-admin.site.register(Comment)
 
