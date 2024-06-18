2e41580cafd560cb594c34780b3d5dbd732a3f51
Musaddek Ali <mussali.one@gmail.com>
2024-06-18 17:36:27 +0000

    Create film_detail.html and to views and and url.

--- film/urls.py
+++ film/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.FilmList.as_view(), name='home'),
+    path('<slug:slug>/', views.film_detail, name='film_detail'),
 ]
--- film/views.py
+++ film/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Movie
 
+
 # Create your views here.
-
-
 class FilmList(generic.ListView):
     queryset = Movie.objects.all()
     template_name = "film/index.html"
     paginate_by = 3
+
+
+def film_detail(request, slug):
+    queryset = Movie.objects.all()
+    film = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "film/film_detail.html",
+        {"film": film},
+    )
