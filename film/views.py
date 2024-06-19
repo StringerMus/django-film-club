22baabee30c6e147757f1da7e5be2b326ac14a6a
Musaddek Ali <mussali.one@gmail.com>
2024-06-19 14:33:49 +0000

    Create Review in forms.py, add to films_details.html for auth users and feedback message on submit.

--- film/views.py
+++ film/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Movie, Review
+from .forms import ReviewForm
 
 
-# Create your views here.
 class FilmList(generic.ListView):
     queryset = Movie.objects.all()
     template_name = "film/index.html"
@@ -16,6 +17,20 @@ def film_detail(request, slug):
     reviews = film.reviews.all().order_by("-created_on")
     review_count = film.reviews.filter().count()
 
+    if request.method == "POST":
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.author = request.user
+            review.post = post
+            review.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your review has been posted'
+    )
+
+    review_form = ReviewForm()
+
     return render(
         request,
         "film/film_detail.html",
@@ -23,5 +38,6 @@ def film_detail(request, slug):
             "movie": film,
             "review": reviews,
             "review_count": review_count,
+            "review_form": review_form,
         },
     )
