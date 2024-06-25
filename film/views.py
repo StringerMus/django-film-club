e777ea27e435fd8e2b239f43a2ce4e574119151a
Musaddek Ali <mussali.one@gmail.com>
2024-06-25 21:13:47 +0000

    Edit spacing on banner and add tagline.

--- film/views.py
+++ film/views.py
@@ -73,7 +73,7 @@ def review_edit(request, slug, review_id):
 #delete reviews
 def review_delete(request, slug, review_id):
     """
-    view to delete review
+    view to delete reviews
     """
     queryset = Movie.objects.all()
     film = get_object_or_404(queryset, slug=slug)
