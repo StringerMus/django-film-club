a9089b7006672513ec9fa74afd4ea05ecefbc0f5
Musaddek Ali <mussali.one@gmail.com>
2024-06-27 15:03:13 +0000

    Invalid test for review form - tests pass.

--- film/tests_forms.py
+++ film/tests_forms.py
@@ -10,4 +10,31 @@ class TestReviewForm(TestCase):
             'content': 'Good film'
             })
 
-        self.assertTrue(review_form.is_valid())
+        self.assertTrue(review_form.is_valid(), msg='Form is invalid')
+
+
+    def test_form_is_invalid(self):
+        review_form = ReviewForm({
+            'title': '',
+            'content': 'Good film'
+            })
+
+        self.assertFalse(review_form.is_valid(), msg='Form is valid')
+
+
+    def test_form_is_invalid(self):
+        review_form = ReviewForm({
+            'title': 'Snap',
+            'content': ''
+            })
+
+        self.assertFalse(review_form.is_valid(), msg='Form is valid')
+
+
+    def test_form_is_invalid(self):
+        review_form = ReviewForm({
+            'title': '',
+            'content': ''
+            })
+
+        self.assertFalse(review_form.is_valid(), msg='Form is valid')
