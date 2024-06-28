f8788e3678818a30cfab4c2fe39130dad650b7a9
Musaddek Ali <mussali.one@gmail.com>
2024-06-28 17:15:02 +0000

    Testing deployment in Heroku due to error.

--- film_club/settings.py
+++ film_club/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-stringermus-djangofilmc-q5xor8d107w.ws-eu114.gitpod.io', '.herokuapp.com', '8000-stringermus-djangofilmc-e2rtn22z9im.ws.codeinstitute-ide.net'
@@ -110,6 +110,14 @@ WSGI_APPLICATION = 'film_club.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
