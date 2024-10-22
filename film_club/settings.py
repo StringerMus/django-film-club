6dadd91daf42cd0cb517d0d6cb9a6dea0f417570
Musaddek Ali <mussali.one@gmail.com>
2024-10-22 16:53:27 +0000

    Debug false -  Deploying for testing.

--- film_club/settings.py
+++ film_club/settings.py
@@ -20,7 +20,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-stringermus-djangofilmc-of0bap27vmt.ws.codeinstitute-ide.net', '.herokuapp.com'
